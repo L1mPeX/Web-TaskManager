1ad6d1c6f18758850b73ddadfabad8ea3f16774c
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-05-03 11:40:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from bottle import route, run, view, static_file, redirect, request
+# server.py
+from bottle import route, run, static_file, view
 
 
 class TodoItem:
@@ -6,19 +7,13 @@ class TodoItem:
         self.description = description
         self.is_completed = False
 
-@route('/static/<filename:path>')
+    def __str__(self):
+        return self.description.lower()
+
+
+@route("/static/<filename:path>")
 def send_static(filename):
     return static_file(filename, root="static")
-
-@route("/add-tesk", method="POST")
-def add_task():
-    desc = request.POST.description.strip()
-    if len(desc) > 0:
-        new_uid = max(tasks_db.keys()) + 1
-        t = TodoItem(desc, new_uid)
-        tsks_db[new_uid] = t
-    return redirect("/")
-
 
 
 @route("/")
@@ -27,11 +22,9 @@ def index():
     tasks = [
         TodoItem("Read the book"),
         TodoItem("Write some code"),
-        TodoItem("Have breakfast"),
     ]
     return {"tasks": tasks}
 
 
-
 ###
-run(host="localhost", port=8080)
+run(host="localhost", port=8080, autoreload=True)
