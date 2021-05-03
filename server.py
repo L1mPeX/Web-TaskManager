d80983b63443f2d561b048dc209f8e8fbd277661
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-05-03 11:36:26 +0300

    Almost added the ability to create tasks

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from bottle import route, run, view, static_file
+from bottle import route, run, view, static_file, redirect, request
 
 
 class TodoItem:
@@ -9,6 +9,16 @@ class TodoItem:
 @route('/static/<filename:path>')
 def send_static(filename):
     return static_file(filename, root="static")
+
+@route("/add-tesk", method="POST")
+def add_task():
+    desc = request.POST.description.strip()
+    if len(desc) > 0:
+        new_uid = max(tasks_db.keys()) + 1
+        t = TodoItem(desc, new_uid)
+        tsks_db[new_uid] = t
+    return redirect("/")
+
 
 
 @route("/")
