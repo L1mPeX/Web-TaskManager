a99fb0752e7824f055f9ecc48149a55a81bc0a02
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-05-04 15:20:58 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
-# server.py
-from bottle import route, run, static_file, view
+from bottle import route, run, view, static_file, redirect
 
 
 class TodoItem:
-    def __init__(self, description):
+    def __init__(self, description, unique_uid):
         self.description = description
         self.is_completed = False
+        self.uid = unique_uid
 
     def __str__(self):
-        return self.description.lower()
+    	return self.description.lower()
 
+
+tasks_db = {
+	1: TodoItem("Read the book", 1),
+	2: TodoItem("Sleep", 2),
+}
 
 @route("/static/<filename:path>")
 def send_static(filename):
@@ -19,12 +24,19 @@ def send_static(filename):
 @route("/")
 @view("index")
 def index():
-    tasks = [
-        TodoItem("Read the book"),
-        TodoItem("Write some code"),
-    ]
+	tasks = tasks_db.values()
     return {"tasks": tasks}
+
+@route("api/delete/<uid:int>")
+def api_delete(uid):
+	tasks_db.pop(uid)
+	return redirect("/")
+
+@route("api/complete/<uid:int>")
+def api_comlete(uid):
+	tasks_db[uid].is_completed = True
+	return "Ok"
 
 
 ###
-run(host="localhost", port=8080, autoreload=True)
+run(host="localhost", port=8080)
